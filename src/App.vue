fed76e0184727431da866587b901fcacc2d71b25
sangeeky <sangeeky@outlook.com>
2019-06-24 16:48:37 -0500

    Diseño Taxis

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" style="width:50px; height:50px" class="mx-auto d-block">
+    <TaxisClientes /> 
+    <Taxis/>
+  </div>
+</template>
+
+<script>
+import TaxisClientes from './components/TaxisCliente.vue'
+import Taxis from './components/Taxis.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TaxisClientes,
+    Taxis
+  }
+}
+
+
+</script>
+
+
---
+++ src/components/Taxis.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h4>Lista du Taxis</h4>
+            <div class="col-md-12">
+                <ul v-for="item of taxis" :key="item.id" class="list-group">
+                <li class="list-group-item">{{ item.placa }} - {{ item.marca }} - {{ item.estado}} </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'Taxis',
+    data (){
+        return{
+            taxis:[
+                {placa:'SXA-987', marca: 'Nissan', estado:true},
+                {placa:'CPB-353', marca: 'Chevrolet', estado:true},
+                {placa:'NPA-934', marca: 'Toyota', estado:true}
+            ]
+               
+        }
+    }
+}
+
+</script>
