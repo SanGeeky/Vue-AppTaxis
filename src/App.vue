4ac9f0fb5e9d7796ad8b28bf241bdb03d55da4c4
David Pinchao <sangeeky@outlook.com>
2019-06-26 02:40:26 -0500

    App Funcional Precios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" style="width:50px; height:50px" class="mx-auto d-block">
-    <TaxisClientes /> 
+    <Clientes/> 
     <Taxis/>
   </div>
 </template>
 
 <script>
-import TaxisClientes from './components/TaxisCliente.vue'
+import Clientes from './components/Clientes.vue'
 import Taxis from './components/Taxis.vue'
 
 export default {
   name: 'app',
   components: {
-    TaxisClientes,
+    Clientes,
     Taxis
   }
 }
